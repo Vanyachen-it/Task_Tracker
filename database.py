980ff6b19d35d217d8ac2cec427ed2e1ca22a7ea
Vanyachen-it <tarasenkoivan10000@mail.ru>
2026-05-13 17:07:40 +0700

    Delete database.py

--- database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-class Manager:
-    _instance = None
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(Manager, cls).__new__(cls)
-            cls._instance.conn = sqlite3.connect('taskflow.db', check_sane_thread=False)
-            
