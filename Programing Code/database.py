6919f810a0f926cedb99023542abe81a1e7a23c0
Vanyachen-it <tarasenkoivan10000@mail.ru>
2026-05-13 18:17:19 +0700

    Update database.py
    Дополнил код для БД

--- Programing Code/database.py
+++ Programing Code/database.py
@@ -5,5 +5,15 @@ class Manager:
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(Manager, cls).__new__(cls)
-            cls._instance.conn = sqlite3.connect('taskflow.db', check_sane_thread=False)
+            cls._instance.conn = sqlite3.connect(
+                'taskflow.db',
+                check_sane_thread=False
+            )
+            cls._instance.create_tables()
+        return cls._instance
+    def create_tables(self):
+        cursor = self.conn.cursor()
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+        id
             
