e2450f3b17f5a12c751af46feb8e3978030b214e
Vanyachen-it <tarasenkoivan10000@mail.ru>
2026-05-20 16:32:43 +0700

    Update database.py
    Снова провел работу над кодом

--- Programing Code/database.py
+++ Programing Code/database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import threading
 
 class Manager:
     _instance = None
@@ -10,8 +11,10 @@ class Manager:
                 check_sane_thread=False
             )
             cls._instance.create_tables()
-
-create_table(self):
+            cls._instance.create_tables()
+        return cls._instance
+        
+def create_table(self):
 cursor = self.conn.cursor()
 
 cursor.execute ("""
@@ -35,4 +38,24 @@ cursor.execute("""
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS tasks (
-id .
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+task_name TEXT MOT NULL,
+status TEXT NOT NULL DEFAULT 'NEW',
+project_id INTEGER NOT NULL,
+executor_id INTEGER,
+FOREIGN KEY (project_id) REFERENCES project(id) ON DELETE CASCADE,
+FOREIGN KEY (executor_id) REFERENCES users(id)
+);
+""")
+
+self.conn.commit()
+
+def execute_secure(self, query, params =()):
+    cursor = self.conn.cursor()
+    try:
+        cursor.execute(query, params)
+        self.conn.commit()
+        return cursor
+    except sqlite3.Error as e:
+        print(self.conn.rollback())
+        raise e
