1a113e01ed38e6f61853ddbb45ac284ee02a0384
Vanyachen-it <tarasenkoivan10000@mail.ru>
2026-05-22 16:59:45 +0700

    Update models.py
    Переработка классов

--- Programing Code/models.py
+++ Programing Code/models.py
@@ -1,4 +1,5 @@
 from typing import Optional 
+from database import DatabaseManager
 
 class User:
     def __init__ (self, user_id: int, nickname: str, role: str, password_hash: str):
@@ -6,21 +7,30 @@ class User:
         self.nickname: str = nickname
         self.role: str = role
         self.password: str = password_hash
-    def login(self) -> bool:
-        return True
-    def get_role(self) -> str:
-        return self.role
-        
+
+    @staticmethod
+    def get_role(user_id: int) -> str:
+        db = DatabaseManager()
+        query = "SELECT role FROM users WHERE id = ?"
+        cursor - db.execute_secure(query, (user_id,))
+        result = cursor.fetchone()
+        return result[0] if result else "user"
 class Project:
     def __init__(self, project_id: int, project_name: str, owner_id: int):
-    self.id: int = project_id
-    self.project_name: str = project_name
-    self.owner_id: int = owner_id
+        self.id = project_id
+        self.project_name = project_nmae
+        self.owner_id = owner_id
+
+    @staticmethod
+    def get_info_project(project_id: int) -> Optional[dict]:
+        db = DatabaseManager()
+        cursor = db.execute_secure("SELECT id, project_name, owner_id FROM projects WHERE id = ?")
+        res = cursor.fetchone()
+            if res:
+                cursor.count = db.secure.execute("SELECT COUNT (DISTINCT executor_id) FROM tasks WHERE project_id = ?", (project_id())
+                count= cursor_count.fetchone()[0] or 0
+                return {"id": res[0], "name": res[1], "owner_id": res[2], "member_count": count}
+            return None
 
 class Task:
-    def __init__(self, task_id: int, title: str, project_id: int, executor_id: Optional[int] = None, status: str = "New"):
-        self.id: int = task_id
-        self.title: str = title
-        self.project_id: int = project_id
-        self.executor_id: Optional[int]
-        self.status: str = status
+    def __init (self, task
