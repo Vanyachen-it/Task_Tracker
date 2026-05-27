702aa5d491ff479e7caeab9cffab035ba9cdf569
Vanyachen-it <tarasenkoivan10000@mail.ru>
2026-05-27 18:05:51 +0700

    Update models.py

--- Programing Code/models.py
+++ Programing Code/models.py
@@ -1,36 +1,13 @@
-from typing import Optional 
-from database import DatabaseManager
+class TaskEntity:
+    def __init__(self, title, project_id, priority, status="New"):
+        self.title = title
+        self.project_id = project_id
+        self.priority = priority
+        self.status = status 
 
-class User:
-    def __init__ (self, user_id: int, nickname: str, role: str, password_hash: str):
-        self.id: int = user_id
-        self.nickname: str = nickname
-        self.role: str = role
-        self.password: str = password_hash
-
+class EntityFactory:
     @staticmethod
-    def get_role(user_id: int) -> str:
-        db = DatabaseManager()
-        query = "SELECT role FROM users WHERE id = ?"
-        cursor - db.execute_secure(query, (user_id,))
-        result = cursor.fetchone()
-        return result[0] if result else "user"
-class Project:
-    def __init__(self, project_id: int, project_name: str, owner_id: int):
-        self.id = project_id
-        self.project_name = project_nmae
-        self.owner_id = owner_id
-
-    @staticmethod
-    def get_info_project(project_id: int) -> Optional[dict]:
-        db = DatabaseManager()
-        cursor = db.execute_secure("SELECT id, project_name, owner_id FROM projects WHERE id = ?")
-        res = cursor.fetchone()
-            if res:
-                cursor.count = db.secure.execute("SELECT COUNT (DISTINCT executor_id) FROM tasks WHERE project_id = ?", (project_id())
-                count= cursor_count.fetchone()[0] or 0
-                return {"id": res[0], "name": res[1], "owner_id": res[2], "member_count": count}
-            return None
-
-class Task:
-    def __init (self, task
+    def create(entity_type: str, **kwargs):
+        if entity_type == "task":
+            return TaskEntity(kwargs.get("title"), kwargs.get("project_id"), kwargs.get("priority"))
+        raise ValueError(f"Фабрика не может создать тип: {entity_type}")
