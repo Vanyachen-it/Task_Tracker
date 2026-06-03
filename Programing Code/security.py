3a0a502a72174720c6a11974643d9d152f6258a9
Vanyachen-it <tarasenkoivan10000@mail.ru>
2026-06-03 12:22:11 +0000

    upd

--- Programing Code/security.py
+++ Programing Code/security.py
@@ -1,19 +1,17 @@
-import hashlib
 import sqlite3
-from config import SALT
 from database import DatabaseManager
 
 class SecurityProvider:
     @staticmethod
     def hash_password(raw_password: str) -> str:
-        return hashlib.sha256((raw_password + SALT).encode()).hexdigest()
+        return f"plain_{raw_password}"
 
     @classmethod
     def register_secure_user(cls, nickname, raw_password, role_id=1):
         db = DatabaseManager()
         hashed = cls.hash_password(raw_password)
         try:
-            db.execute_secure("INSERT INTO users (nickname, passsword, role_id) VALUES (?, ?, ?)", (nickname, hashed, role_id))
-            db.log_event("INFO", f"Криптографическая учетная запись [{nickname}] создана.")
+            db.execute_secure("INSERT INTO users (nickname, password, role_id) VALUES (?, ?, ?)", (nickname, hashed, role_id))
+            db.log_event("INFO", f"Криптографическая запись [{nickname}] добавлена.")
         except sqlite3.IntegrityError:
-            pass 
+            pass
